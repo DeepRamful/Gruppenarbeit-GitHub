e46a5751bf8cb2419e1b4b882ae064799245dfb4
Huyizii <blue14899@gmail.com>
2023-01-21 23:51:55 +0100

    New Dataset 2
    data.csv 3

--- Skript1.R
+++ Skript1.R
@@ -1,7 +1,7 @@
 #Skript 1
 ID <- sample(120000:220000,size= 100,replace = FALSE,prob=NULL)
 #1)
-Alter <- rnorm(100, 25, 2)
+Alter <- round(rnorm(100, 25, 2))
 
 
 #2)
@@ -27,7 +27,7 @@ Interesse_an_Programmieren <- sample(values, size = 100, replace = TRUE)
 #5)
 values <- c("ja", "nein") 
 eachProb <- c(0.5,0.5) 
-Mathe_LK <-  sample(values, size = 100, replace = TRUE, prob = prob)
+Mathe_LK <-  sample(values, size = 100, replace = TRUE, prob = eachProb)
 
 Data <- data.frame(ID,Alter,Studienfach,Interesse_an_Mathematik,Interesse_an_Programmieren,Mathe_LK)
 write.csv(Data,"C:\\Users\\Quang Huy\\Desktop\\3.Semester\\W.Arbeit\\GitHub\\Gruppenarbeit-GitHub\\Data.csv",row.names=FALSE)
