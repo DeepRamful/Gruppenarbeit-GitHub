c138fb24935d68210d79f9058c81c0ec73b57edd
DeepRamful <119403368+DeepRamful@users.noreply.github.com>
2023-01-23 23:36:51 +0100

    Function (f) added

--- Skript2.R
+++ Skript2.R
@@ -1,6 +1,7 @@
 #Skript 2
 #Funktionen
 
+df <- read.csv("https://raw.githubusercontent.com/DeepRamful/Gruppenarbeit-GitHub/main/Data.csv")
 #a)
 
 
@@ -31,7 +32,14 @@
 
 
 #f)
+# Choose 3 categorical Variables
+threeVar <- table(df$Studienfach, df$Interesse_an_Mathematik, df$Mathe_LK)
 
+# Create the Mosaic Plot
+mosaicplot(threeVar, main = "Studienfach, Interesse an Mathe und Mathe-Lk Mosaic Plot",
+           ylab = "Interesse an Mathe (7 = sehr hohes Interesse)",
+           xlab = "Mathe LK(Ja/Nein)",
+           col = c("blue","red"))
 
 
 
