aec5a990dba4f20a457d9bbabe8133737252c923
Huyizii <blue14899@gmail.com>
2023-01-24 14:52:28 +0100

    Add function a and b
    Function a and b

--- Skript2.R
+++ Skript2.R
@@ -2,14 +2,27 @@
 #Funktionen
 
 #a)
+#Geometrisches Mittel berechnen:
+#Create the function
+gmittel <- function(a,b){
+  x <- prod(a)^(1/b)
+  return(x)
+}
 
-
-
-
+#Add the variable to the function
+gmittel(df$Alter,length(df$Alter))
 
 #b)
+#Modus berechnen:
+#Create function
+modus <- function(x){
+  y <- unique(x)
+  z <- y[which.max(tabulate(match(x,y)))]
+  return(z)
+}
 
-
+#Add the variable to the function
+modus(df$Studienfach)
 
 
 #c)
