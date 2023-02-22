f6369849b897557cbd3708455ee653091cf9dd39
DeepRamful <119403368+DeepRamful@users.noreply.github.com>
2023-02-22 21:27:14 +0100

    Made (f) a function

--- Skript2.R
+++ Skript2.R
@@ -76,14 +76,15 @@ quant.Prog.Ineresse <- quantilbasierte_kategorisierung(df$Interesse_an_Programmi
 
 
 #f)
-# Choose 3 categorical Variables
-threeVar <- table(df$Studienfach, df$Interesse_an_Mathematik, df$Mathe_LK)
-
-# Create the Mosaic Plot
-mosaicplot(threeVar, main = "Studienfach, Interesse an Mathe und Mathe-Lk Mosaic Plot",
+visualisierungKatVar <- function(data){
+  # Choose 3 categorical Variables
+  threeVar <- table(data$Studienfach, data$Interesse_an_Mathematik, data$Mathe_LK)
+  # Create the Mosaic Plot
+  mosaicplot(threeVar, main = "Studienfach, Interesse und Mathe and Mathe Lk Mosaic Plot",
            ylab = "Interesse an Mathe (7 = sehr hohes Interesse)",
            xlab = "Mathe LK(Ja/Nein)",
            col = c("blue","red"))
+}
 
 
 
